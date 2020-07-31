5d60bdba6c0ed0294f80e1987038ac6c3534d423
Edoardo Riggio <edo.riggio19@gmail.com>
2020-07-31 20:24:37 +0200

    modified sh file

--- mkicon.sh
+++ mkicon.sh
@@ -2,53 +2,50 @@
 
 # Copyright 2020 Edoardo Riggio
 # 
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 # 
-#        http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 # 
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#!/bin/sh
+while getopts 'ih' flag; do
+  case "${flag}" in
+    i) ICNS='y' ;;
+    h) HELP='true'
+       echo "mkicon [options] image destination"
+       echo " "
+       echo "options:"
+       echo "-h         show brief help"
+       echo "-f         create a .icns file"
+       exit 0 ;;
+    \?) exit 42 ;;
+  esac
+done
 
-mkicon() {
-    FILE=$1
-    DEST=$2
-    ICNS=$3
-
-    mkdir $DEST
-    ~/generate_icns.py $FILE $DEST
-
-    if [[ $ICNS == 'y' ]] || [[ $ICNS == 'Y' ]]
-    then
-        iconutil -c icns $DEST
-    fi
-}
-
-DIRNAME='/icon.iconset'
-ICON='y'
-
-echo 'Specify the image path:'
-read path
-
-echo '----------------------------'
-echo 'Specify the destination path:'
-read dest
-
-DIRPATH=$dest$DIRNAME
-
-echo '-----------------------------------'
-echo 'Do you want a .icns file too? [y/n]'
-read icon
-
-if [ $icon != $ICON ]
+if [[ $# < 2 ]]
 then
-    ICON=$icon
+    echo Both an image path and a destination path must be passed to mkicon
+    exit 1
 fi
 
-mkicon $path $DIRPATH $ICON
+shift $((OPTIND - 1))
+
+FILE=$1
+DEST=$2
+
+DIRNAME='/icon.iconset'
+DESTDIR=$DEST$DIRNAME
+
+mkdir $DESTDIR
+./generate_icns.py $FILE $DESTDIR
+
+if [[ $ICNS == 'y' ]]
+then
+    iconutil -c icns $DESTDIR
+fi
